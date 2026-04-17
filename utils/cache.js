cb5c0c49beabe20148e27b0da0fc14df7f237524
Raby012 <fleentbisht@gmail.com>
2026-04-17 14:43:18 +0530

    Update cache.js

--- utils/cache.js
+++ utils/cache.js
@@ -1,1 +1,8 @@
+const NodeCache = require('node-cache');
+const cache = new NodeCache({ stdTTL: 3600 }); // 1 hour cache
 
+module.exports = {
+  get: (key) => cache.get(key),
+  set: (key, value) => cache.set(key, value),
+  has: (key) => cache.has(key)
+};
